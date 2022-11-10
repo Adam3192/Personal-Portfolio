6afefbef0f87991fd1815a0aae9d60a0f4d82f02
Adam3192 <jeremyadixon@hotmail.com>
2022-11-10 14:29:35 -0600

    Personal Portfolio Website

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from '../assets/logo.png';
+
+export const ProjectList = [
+ {
+   name: 'Project 1',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ },
+
+ {
+   name: 'Project 2',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ },
+
+ {
+   name: 'Project 3',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ },
+
+ {
+   name: 'Project 4',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ },
+
+ {
+   name: 'Project 5',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ },
+
+ {
+   name: 'Project 6',
+   image: Proj1,
+   skills: 'React, Node.js, MongoDB'
+ }
+]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import EmailIcon from '@material-ui/icons/Email'
+import GithubIcon from '@material-ui/icons/GitHub'
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Adam</h2>
+        <div className="prompt">
+          {' '}
+          <p>A software developer with a passion for learning and creating!</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className='list'>
+           <li className='item'>
+            <h2>Front-End</h2>
+            <span>ReactJS, HTML, CSS, Ionic, NPM, BootStrap, </span>
+           </li>
+           <li className='item'>
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS, GraphQL, ApolloServer, MySQL, MongoDB, AWS S3</span>
+           </li>
+           <li className='item'>
+            <h2>Languages</h2>
+            <span>JavaScript, TypeScript</span>
+           </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
